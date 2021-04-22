0572b541a66f4130c05a7691024841b424b2c7f5
Guilherme Felipe Martins <guilherme.martins92@outlook.com>
2021-04-21 21:42:10 -0300

    SellerService e FindAll Sincrono

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -11,7 +11,7 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Models;
 using SalesWebMvc.Data;
-
+using SalesWebMvc.Services;
 
 namespace SalesWebMvc
 {
@@ -34,6 +34,8 @@ namespace SalesWebMvc
                     builder.MigrationsAssembly("SalesWebMvc")));
 
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
